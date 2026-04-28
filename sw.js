fde29ecfab0cbbeacf311737b2422d6ebbc40ddf
Preston Wilson <pdlw-github-com@linkbeat.com>
2026-04-28 07:11:44 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,63 +1,45 @@
-const CACHE_NAME = 'st-michael-v4';
-
-// List of all the exact files the app needs to run completely offline
-const URLS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.png',
-  './opening.mp3',
-  './1.mp3',
-  './2.mp3',
-  './3.mp3',
-  './4.mp3',
-  './5.mp3',
-  './6.mp3',
-  './7.mp3',
-  './8.mp3',
-  './9.mp3',
-  './0.mp3'
+const CACHE_NAME = 'stmichael-v5';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png',
+  '/chaplet.m4a'
 ];
 
-// 1. When the app loads for the first time, download everything to local storage
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache and saving files to local storage');
-        return cache.addAll(URLS_TO_CACHE);
-      })
-  );
-  self.skipWaiting();
-});
-
-// 2. When the app asks for a file (like an MP3), serve it from the local hard drive, NOT the internet
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // If the file is in local storage, return it immediately
-        if (response) {
-          return response;
-        }
-        // Otherwise, fetch from the internet just in case
-        return fetch(event.request);
+        return cache.addAll(urlsToCache);
       })
   );
 });
 
-// 3. Clean up old caches if we ever update the version number
 self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
         })
       );
     })
   );
-  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
